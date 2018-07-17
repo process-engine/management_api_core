344ae7cd1e476228ea95ce6d1b78a3ecd2da63b0
5ebastianMeier <sebastian.meier@5minds.de>
2018-07-17 15:09:29 +0200

    fix typo

--- src/converters/event_converter.ts
+++ src/converters/event_converter.ts
@@ -2,9 +2,9 @@ import {Event} from '@process-engine/management_api_contracts';
 import {Model} from '@process-engine/process_engine_contracts';
 
 export function managementApiEventConverter(event: Model.Events.Event): Event {
-  const consumerApiEvent: Event = new Event();
-  consumerApiEvent.key = event.id;
-  consumerApiEvent.id = event.id;
+  const managementApiEvent: Event = new Event();
+  managementApiEvent.key = event.id;
+  managementApiEvent.id = event.id;
 
-  return consumerApiEvent;
+  return managementApiEvent;
 }
