8d04915b9f3a10e39c0e82cc86175d595a2f0f17
ElRaptorus <christian.werner@5minds.de>
2018-07-26 11:16:39 +0200

    Use deployment api for updating a process model by its id

--- ioc_module.js
+++ ioc_module.js
@@ -14,6 +14,7 @@ function registerInContainer(container) {
     .register('ManagementApiService', ManagementApiService)
     .dependencies(
       'CorrelationService',
+      'DeploymentApiService',
       'EventAggregator',
       'ExecutionContextFacadeFactory',
       'FlowNodeInstanceService',
