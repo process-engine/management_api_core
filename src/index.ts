c254c10a9c3ccca6d475d4424dc6feb85330394b
ElRaptorus <christian.werner@5minds.de>
2018-07-27 10:26:44 +0200

    Use ConsumerApiService for processmodel- and usertask- related operations

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import * as processModelExecution from './process_model_execution/index';
-
-// tslint:disable:no-namespace
-export namespace ManagementApi {
-  export import ProcessModelExecution = processModelExecution;
-}
-
 export * from './management_api_service';
 export * from './converters';
--- src/management_api_service.ts
+++ src/management_api_service.ts
@@ -1,6 +1,9 @@
-import * as EssentialProjectErrors from '@essential-projects/errors_ts';
-import {IEventAggregator} from '@essential-projects/event_aggregator_contracts';
 import {IIdentity} from '@essential-projects/iam_contracts';
+import {
+  IConsumerApiService,
+  ProcessModel as ConsumerApiProcessModel,
+  ProcessModelList as ConsumerApiProcessModelList,
+} from '@process-engine/consumer_api_contracts';
 import {DeploymentContext, IDeploymentApiService, ImportProcessModelRequestPayload} from '@process-engine/deployment_api_contracts';
 import {
   Correlation,
@@ -9,7 +12,6 @@ import {
   IManagementApiService,
   ManagementContext,
   ProcessModelExecution,
-  UserTask,
   UserTaskList,
   UserTaskResult,
 } from '@process-engine/management_api_contracts';
@@ -18,7 +20,6 @@ import {
   ICorrelationService,
   IExecutionContextFacade,
   IExecutionContextFacadeFactory,
-  IFlowNodeInstanceService,
   IProcessModelFacade,
   IProcessModelFacadeFactory,
   IProcessModelService,
@@ -28,42 +29,36 @@ import {
 } from '@process-engine/process_engine_contracts';
 
 import * as Converters from './converters/index';
-import {IProcessModelExecutionAdapter} from './process_model_execution/index';
 
 import * as BluebirdPromise from 'bluebird';
 
 export class ManagementApiService implements IManagementApiService {
   public config: any = undefined;
 
+  private _consumerApiService: IConsumerApiService;
   private _correlationService: ICorrelationService;
   private _deploymentApiService: IDeploymentApiService;
-  private _eventAggregator: IEventAggregator;
   private _executionContextFacadeFactory: IExecutionContextFacadeFactory;
-  private _flowNodeInstanceService: IFlowNodeInstanceService;
   private _processModelFacadeFactory: IProcessModelFacadeFactory;
-  private _processModelExecutionAdapter: IProcessModelExecutionAdapter;
   private _processModelService: IProcessModelService;
 
-  private convertProcessModel: Function;
-  private convertUserTasks: Function;
-
-  constructor(correlationService: ICorrelationService,
+  constructor(consumerApiService: IConsumerApiService,
+              correlationService: ICorrelationService,
               deploymentApiService: IDeploymentApiService,
-              eventAggregator: IEventAggregator,
               executionContextFacadeFactory: IExecutionContextFacadeFactory,
-              flowNodeInstanceService: IFlowNodeInstanceService,
               processModelFacadeFactory: IProcessModelFacadeFactory,
-              processModelExecutionAdapter: IProcessModelExecutionAdapter,
               processModelService: IProcessModelService) {
 
+    this._consumerApiService = consumerApiService;
     this._correlationService = correlationService;
     this._deploymentApiService = deploymentApiService;
-    this._eventAggregator = eventAggregator;
     this._executionContextFacadeFactory = executionContextFacadeFactory;
-    this._flowNodeInstanceService = flowNodeInstanceService;
-    this._processModelExecutionAdapter = processModelExecutionAdapter;
     this._processModelFacadeFactory = processModelFacadeFactory;
     this._processModelService = processModelService;
+  }
+
+  private get consumerApiService(): IConsumerApiService {
+    return this._consumerApiService;
   }
 
   private get correlationService(): ICorrelationService {
@@ -74,20 +69,8 @@ export class ManagementApiService implements IManagementApiService {
     return this._deploymentApiService;
   }
 
-  private get eventAggregator(): IEventAggregator {
-    return this._eventAggregator;
-  }
-
   private get executionContextFacadeFactory(): IExecutionContextFacadeFactory {
     return this._executionContextFacadeFactory;
-  }
-
-  private get flowNodeInstanceService(): IFlowNodeInstanceService {
-    return this._flowNodeInstanceService;
-  }
-
-  private get processModelExecutionAdapter(): IProcessModelExecutionAdapter {
-    return this._processModelExecutionAdapter;
   }
 
   private get processModelFacadeFactory(): IProcessModelFacadeFactory {
@@ -96,14 +79,6 @@ export class ManagementApiService implements IManagementApiService {
 
   private get processModelService(): IProcessModelService {
     return this._processModelService;
-  }
-
-  public async initialize(): Promise<void> {
-
-    this.convertProcessModel = Converters.createProcessModelConverter(this.processModelFacadeFactory);
-    this.convertUserTasks = Converters.createUserTaskConverter(this.processModelFacadeFactory, this.processModelService);
-
-    return Promise.resolve();
   }
 
   // Correlations
@@ -121,13 +96,13 @@ export class ManagementApiService implements IManagementApiService {
   public async getProcessModels(context: ManagementContext): Promise<ProcessModelExecution.ProcessModelList> {
 
     const executionContextFacade: IExecutionContextFacade = await this._createExecutionContextFacadeFromManagementContext(context);
-    const processModels: Array<Model.Types.Process> = await this.processModelService.getProcessModels(executionContextFacade);
+    const consumerApiProcessModels: ConsumerApiProcessModelList = await this.consumerApiService.getProcessModels(context);
 
     const managementApiProcessModels: Array<ProcessModelExecution.ProcessModel> =
-      await BluebirdPromise.map(processModels, async(processModel: Model.Types.Process) => {
+      await BluebirdPromise.map(consumerApiProcessModels.processModels, async(processModel: ConsumerApiProcessModel) => {
         const processModelRaw: string = await this._getRawXmlForProcessModelById(executionContextFacade, processModel.id);
 
-        return this.convertProcessModel(processModel, processModelRaw);
+        return Converters.convertProcessModel(processModel, processModelRaw);
       });
 
     return <ProcessModelExecution.ProcessModelList> {
@@ -139,10 +114,10 @@ export class ManagementApiService implements IManagementApiService {
 
     const executionContextFacade: IExecutionContextFacade = await this._createExecutionContextFacadeFromManagementContext(context);
 
-    const processModel: Model.Types.Process = await this.processModelService.getProcessModelById(executionContextFacade, processModelId);
-    const processModelRaw: string = await this._getRawXmlForProcessModelById(executionContextFacade, processModel.id);
+    const consumerApiProcessModel: ConsumerApiProcessModel = await this.consumerApiService.getProcessModelById(context, processModelId);
+    const processModelRaw: string = await this._getRawXmlForProcessModelById(executionContextFacade, consumerApiProcessModel.id);
 
-    const managementApiProcessModel: ProcessModelExecution.ProcessModel = this.convertProcessModel(processModel, processModelRaw);
+    const managementApiProcessModel: ProcessModelExecution.ProcessModel = Converters.convertProcessModel(consumerApiProcessModel, processModelRaw);
 
     return managementApiProcessModel;
   }
@@ -156,15 +131,7 @@ export class ManagementApiService implements IManagementApiService {
                                     endEventId?: string,
                                   ): Promise<ProcessModelExecution.ProcessStartResponsePayload> {
 
-    const executionContextFacade: IExecutionContextFacade = await this._createExecutionContextFacadeFromManagementContext(context);
-
-    // Uses the standard IAM facade with the processModelService => The process model gets filtered.
-    const processModel: Model.Types.Process = await this.processModelService.getProcessModelById(executionContextFacade, processModelId);
-
-    this._validateStartRequest(processModel, startEventId, endEventId, startCallbackType);
-
-    return this.processModelExecutionAdapter
-      .startProcessInstance(executionContextFacade, processModelId, startEventId, payload, startCallbackType, endEventId);
+    return this.consumerApiService.startProcessInstance(context, processModelId, startEventId, payload, startCallbackType, endEventId);
   }
 
   public async getEventsForProcessModel(context: ManagementContext, processModelId: string): Promise<EventList> {
@@ -204,48 +171,19 @@ export class ManagementApiService implements IManagementApiService {
   // UserTasks
   public async getUserTasksForProcessModel(context: ManagementContext, processModelId: string): Promise<UserTaskList> {
 
-    const executionContextFacade: IExecutionContextFacade = await this._createExecutionContextFacadeFromManagementContext(context);
-
-    await this._checkIfProcessModelInstanceExists(executionContextFacade, processModelId);
-
-    const suspendedFlowNodes: Array<Runtime.Types.FlowNodeInstance> =
-      await this.flowNodeInstanceService.querySuspendedByProcessModel(executionContextFacade, processModelId);
-
-    const userTaskList: UserTaskList = await this.convertUserTasks(executionContextFacade, suspendedFlowNodes);
-
-    return userTaskList;
+    return this.consumerApiService.getUserTasksForProcessModel(context, processModelId);
   }
 
   public async getUserTasksForCorrelation(context: ManagementContext, correlationId: string): Promise<UserTaskList> {
 
-    const executionContextFacade: IExecutionContextFacade = await this._createExecutionContextFacadeFromManagementContext(context);
-
-    await this._checkIfCorrelationExists(executionContextFacade, correlationId);
-
-    const suspendedFlowNodes: Array<Runtime.Types.FlowNodeInstance> =
-      await this.flowNodeInstanceService.querySuspendedByCorrelation(executionContextFacade, correlationId);
-
-    const userTaskList: UserTaskList = await this.convertUserTasks(executionContextFacade, suspendedFlowNodes);
-
-    return userTaskList;
+    return this.consumerApiService.getUserTasksForCorrelation(context, correlationId);
   }
 
   public async getUserTasksForProcessModelInCorrelation(context: ManagementContext,
                                                         processModelId: string,
                                                         correlationId: string): Promise<UserTaskList> {
 
-    const executionContextFacade: IExecutionContextFacade = await this._createExecutionContextFacadeFromManagementContext(context);
-
-    await this._checkIfCorrelationExists(executionContextFacade, correlationId);
-    await this._checkIfProcessModelInstanceExists(executionContextFacade, processModelId);
-
-    const suspendedFlowNodes: Array<Runtime.Types.FlowNodeInstance> =
-      await this.flowNodeInstanceService.querySuspendedByCorrelation(executionContextFacade, correlationId);
-
-    const userTaskList: UserTaskList =
-      await this.convertUserTasks(executionContextFacade, suspendedFlowNodes, processModelId);
-
-    return userTaskList;
+    return this.consumerApiService.getUserTasksForProcessModelInCorrelation(context, processModelId, correlationId);
   }
 
   public async finishUserTask(context: ManagementContext,
@@ -254,30 +192,7 @@ export class ManagementApiService implements IManagementApiService {
                               userTaskId: string,
                               userTaskResult: UserTaskResult): Promise<void> {
 
-    const userTasks: UserTaskList = await this.getUserTasksForProcessModelInCorrelation(context, processModelId, correlationId);
-
-    const userTask: UserTask = userTasks.userTasks.find((task: UserTask) => {
-      return task.key === userTaskId;
-    });
-
-    if (userTask === undefined) {
-      const errorMessage: string = `Process model '${processModelId}' in correlation '${correlationId}' does not have a user task '${userTaskId}'`;
-      throw new EssentialProjectErrors.NotFoundError(errorMessage);
-    }
-
-    const resultForProcessEngine: any = this._getUserTaskResultFromUserTaskConfig(userTaskResult);
-
-    return new Promise<void>((resolve: Function, reject: Function): void => {
-      this.eventAggregator.subscribeOnce(`/processengine/node/${userTask.id}/finished`, (event: any) => {
-        resolve();
-      });
-
-      this.eventAggregator.publish(`/processengine/node/${userTask.id}/finish`, {
-        data: {
-          token: resultForProcessEngine,
-        },
-      });
-    });
+    return this.consumerApiService.finishUserTask(context, processModelId, correlationId, userTaskId, userTaskResult);
   }
 
   private async _createExecutionContextFacadeFromManagementContext(managementContext: ManagementContext): Promise<IExecutionContextFacade> {
@@ -295,80 +210,6 @@ export class ManagementApiService implements IManagementApiService {
       await this.processModelService.getProcessDefinitionAsXmlById(executionContextFacade, processModelId);
 
     return processModelRaw.xml;
-  }
-
-  private _validateStartRequest(processModel: Model.Types.Process,
-                                startEventId: string,
-                                endEventId: string,
-                                startCallbackType: ProcessModelExecution.StartCallbackType,
-                               ): void {
-
-    if (!Object.values(ProcessModelExecution.StartCallbackType).includes(startCallbackType)) {
-      throw new EssentialProjectErrors.BadRequestError(`${startCallbackType} is not a valid return option!`);
-    }
-
-    if (!processModel.isExecutable) {
-      throw new EssentialProjectErrors.BadRequestError('The process model is not executable!');
-    }
-
-    const hasMatchingStartEvent: boolean = processModel.flowNodes.some((flowNode: Model.Base.FlowNode): boolean => {
-      return flowNode.id === startEventId;
-    });
-
-    if (!hasMatchingStartEvent) {
-      throw new EssentialProjectErrors.NotFoundError(`StartEvent with ID '${startEventId}' not found!`);
-    }
-
-    if (startCallbackType === ProcessModelExecution.StartCallbackType.CallbackOnEndEventReached) {
-
-      if (!endEventId) {
-        throw new EssentialProjectErrors.BadRequestError(`Must provide an EndEventId, when using callback type 'CallbackOnEndEventReached'!`);
-      }
-
-      const hasMatchingEndEvent: boolean = processModel.flowNodes.some((flowNode: Model.Base.FlowNode): boolean => {
-        return flowNode.id === endEventId;
-      });
-
-      if (!hasMatchingEndEvent) {
-        throw new EssentialProjectErrors.NotFoundError(`EndEvent with ID '${startEventId}' not found!`);
-      }
-    }
-  }
-
-  private async _checkIfCorrelationExists(executionContextFacade: IExecutionContextFacade, correlationId: string): Promise<void> {
-    const flowNodeInstances: Array<Runtime.Types.FlowNodeInstance> =
-      await this.flowNodeInstanceService.queryByCorrelation(executionContextFacade, correlationId);
-
-    if (!flowNodeInstances || flowNodeInstances.length === 0) {
-      throw new EssentialProjectErrors.NotFoundError(`No Correlation with id '${correlationId}' found.`);
-    }
-  }
-
-  private async _checkIfProcessModelInstanceExists(executionContextFacade: IExecutionContextFacade, processInstanceId: string): Promise<void> {
-    const flowNodeInstances: Array<Runtime.Types.FlowNodeInstance> =
-      await this.flowNodeInstanceService.queryByProcessModel(executionContextFacade, processInstanceId);
-
-    if (!flowNodeInstances || flowNodeInstances.length === 0) {
-      throw new EssentialProjectErrors.NotFoundError(`No process instance with id '${processInstanceId}' found.`);
-    }
-  }
-
-  private _getUserTaskResultFromUserTaskConfig(finishedTask: UserTaskResult): any {
-    const userTaskIsNotAnObject: boolean = finishedTask === undefined
-                                        || finishedTask.formFields === undefined
-                                        || typeof finishedTask.formFields !== 'object'
-                                        || Array.isArray(finishedTask.formFields);
-
-    if (userTaskIsNotAnObject) {
-      throw new EssentialProjectErrors.BadRequestError('The UserTasks formFields is not an object.');
-    }
-
-    const noFormfieldsSubmitted: boolean = Object.keys(finishedTask.formFields).length === 0;
-    if (noFormfieldsSubmitted) {
-      throw new EssentialProjectErrors.BadRequestError('The UserTasks formFields are empty.');
-    }
-
-    return finishedTask.formFields;
   }
 
 }
