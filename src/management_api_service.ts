320f24118455e4334dd4573eafc9d6b333b97a67
Steffen <Steffen.knaup@5minds.de>
2019-01-02 17:17:25 +0100

    :sparkles: Add Get Tokens For ProcessInstance Functionality

--- src/management_api_service.ts
+++ src/management_api_service.ts
@@ -334,6 +334,12 @@ export class ManagementApiService implements IManagementApi {
     return this._kpiApiService.getActiveTokensForCorrelationAndProcessModel(identity, correlationId, processModelId);
   }
 
+  public async getActiveTokensForProcessInstance(identity: IIdentity,
+                                                 processInstanceId: string): Promise<Array<ActiveToken>> {
+
+    return this._kpiApiService.getActiveTokensForProcessInstance(identity, processInstanceId);
+  }
+
   public async getActiveTokensForFlowNode(identity: IIdentity, flowNodeId: string): Promise<Array<ActiveToken>> {
 
     return this._kpiApiService.getActiveTokensForFlowNode(identity, flowNodeId);
@@ -366,6 +372,12 @@ export class ManagementApiService implements IManagementApi {
 
     return this._tokenHistoryApiService.getTokensForCorrelationAndProcessModel(identity, correlationId, processModelId);
 }
+
+  public async getTokensForProcessInstance(identity: IIdentity,
+                                           processInstanceId: string): Promise<TokenHistoryGroup> {
+
+    return this._tokenHistoryApiService.getTokensForProcessInstance(identity, processInstanceId);
+  }
 
   private async _getRawXmlForProcessModelById(identity: IIdentity, processModelId: string): Promise<string> {
 
