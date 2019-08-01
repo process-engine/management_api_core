1c71bc7b2645badd5a7fd7ac4657e4e841051625
ElRaptorus <christian.werner@5minds.de>
2019-08-01 10:04:07 +0200

    :recycle: Update IoC registrations to new schema

--- ioc_module.js
+++ ioc_module.js
@@ -1,24 +1,86 @@
 'use strict';
 
-const ManagementApiService = require('./dist/commonjs/index').ManagementApiService;
+const {
+  CorrelationService,
+  CronjobService,
+  EventService,
+  EmptyActivityService,
+  FlowNodeInstanceService,
+  KpiService,
+  LoggingService,
+  NotificationService,
+  ManualTaskService,
+  ProcessModelService,
+  TokenHistoryService,
+  UserTaskService,
+} = require('./dist/commonjs/index');
 
 function registerInContainer(container) {
 
   container
-    .register('ManagementApiService', ManagementApiService)
+    .register('ManagementApiCorrelationService', CorrelationService)
+    .dependencies('CorrelationService')
+    .singleton();
+
+  container
+    .register('ManagementApiCronjobService', CronjobService)
+    .dependencies('CronjobService', 'CronjobHistoryService')
+    .singleton();
+
+  container
+    .register('ManagementApiEventService', EventService)
+    .dependencies('ConsumerApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiEmptyActivityService', EmptyActivityService)
+    .dependencies('ConsumerApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiFlowNodeInstanceService', FlowNodeInstanceService)
+    .dependencies('FlowNodeInstanceService')
+    .singleton();
+
+  container
+    .register('ManagementApiKpiService', KpiService)
+    .dependencies('KpiApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiLoggingService', LoggingService)
+    .dependencies('LoggingApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiNotificationService', NotificationService)
+    .dependencies('ConsumerApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiManualTaskService', ManualTaskService)
+    .dependencies('ConsumerApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiProcessModelService', ProcessModelService)
     .dependencies(
       'ConsumerApiService',
-      'CorrelationService',
       'CronjobService',
-      'CronjobHistoryService',
       'EventAggregator',
-      'FlowNodeInstanceService',
       'IamService',
-      'KpiApiService',
-      'LoggingApiService',
       'ProcessModelFacadeFactory',
-      'ProcessModelUseCases',
-      'TokenHistoryApiService')
+      'ProcessModelUseCases')
+    .singleton();
+
+  container
+    .register('ManagementApiTokenHistoryService', TokenHistoryService)
+    .dependencies('TokenHistoryApiService')
+    .singleton();
+
+  container
+    .register('ManagementApiUserTaskService', UserTaskService)
+    .dependencies('ConsumerApiService')
     .singleton();
 }
 
