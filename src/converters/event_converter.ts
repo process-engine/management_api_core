46b48545e8c6679e139e599ad11d4df8ce99dbe6
ElRaptorus <christian.werner@5minds.de>
2019-08-01 09:39:25 +0200

    :fire: Remove converter pattern

--- src/converters/event_converter.ts
+++
@@ -1,9 +0,0 @@
-import {DataModels as ManagementApiTypes} from '@process-engine/management_api_contracts';
-import {Model} from '@process-engine/process_model.contracts';
-
-export function managementApiEventConverter(event: Model.Events.Event): ManagementApiTypes.Events.Event {
-  const managementApiEvent = new ManagementApiTypes.Events.Event();
-  managementApiEvent.id = event.id;
-
-  return managementApiEvent;
-}
--- src/converters/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './correlation_converter';
-export * from './event_converter';
-export * from './process_model_converter';
