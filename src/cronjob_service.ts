98e2d50b41bcc3e1d8525fc3b92ab7c5332e9222
ElRaptorus <christian.werner@5minds.de>
2019-09-11 15:51:24 +0200

    :sparkles: Add pagination to CronjobService

--- src/cronjob_service.ts
+++ src/cronjob_service.ts
@@ -4,6 +4,8 @@ import {ICronjobHistoryService} from '@process-engine/cronjob_history.contracts'
 import {APIs, DataModels} from '@process-engine/management_api_contracts';
 import {ICronjobService} from '@process-engine/process_engine_contracts';
 
+import {applyPagination} from './paginator';
+
 export class CronjobService implements APIs.ICronjobManagementApi {
 
   private readonly cronjobService: ICronjobService;
@@ -17,23 +19,36 @@ export class CronjobService implements APIs.ICronjobManagementApi {
     this.cronjobService = cronjobService;
   }
 
-  public async getAllActiveCronjobs(identity: IIdentity): Promise<Array<DataModels.Cronjobs.CronjobConfiguration>> {
-    return this.cronjobService.getActive();
+  public async getAllActiveCronjobs(
+    identity: IIdentity,
+    offset: number = 0,
+    limit: number = 0,
+  ): Promise<Array<DataModels.Cronjobs.CronjobConfiguration>> {
+
+    const cronjobs = this.cronjobService.getActive();
+
+    const paginizedCronjobs = applyPagination(cronjobs, offset, limit);
+
+    return paginizedCronjobs;
   }
 
   public async getCronjobExecutionHistoryForProcessModel(
     identity: IIdentity,
     processModelId: string,
     startEventId?: string,
+    offset: number = 0,
+    limit: number = 0,
   ): Promise<Array<DataModels.Cronjobs.CronjobHistoryEntry>> {
-    return this.cronjobHistoryService.getByProcessModelId(identity, processModelId, startEventId);
+    return this.cronjobHistoryService.getByProcessModelId(identity, processModelId, startEventId, offset, limit);
   }
 
   public async getCronjobExecutionHistoryForCrontab(
     identity: IIdentity,
     crontab: string,
+    offset: number = 0,
+    limit: number = 0,
   ): Promise<Array<DataModels.Cronjobs.CronjobHistoryEntry>> {
-    return this.cronjobHistoryService.getByCrontab(identity, crontab);
+    return this.cronjobHistoryService.getByCrontab(identity, crontab, offset, limit);
   }
 
 }
